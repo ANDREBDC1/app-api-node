ea1c70bef66d303afdf31f144a12c9a89eeb77fd
Andre <andre.rian@inventsoftware.com.br>
2021-03-10 18:54:22 -0300

    teste rum migration

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,14 @@ const sequelize = require('./database')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
-//    sequelize
-//   .authenticate()
-//   .then(() => {
-//        res.json({mensagem : 'Connection has been established successfully.'});
-//   })
-//   .catch(err => {
-//       res.json({mensagem: 'Unable to connect to the database:', erro: err});
-//   }); 
+   sequelize
+  .authenticate()
+  .then(() => {
+       res.json({mensagem : 'Connection has been established successfully.'});
+  })
+  .catch(err => {
+      res.json({mensagem: 'Unable to connect to the database:', erro: err});
+  }); 
 })
 
 module.exports = routes;
