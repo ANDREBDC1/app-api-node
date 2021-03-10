bd26966efb7952e061008b324e97ad34407ee596
Andre <andre.rian@inventsoftware.com.br>
2021-03-10 18:53:25 -0300

    teste rum migration

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require('express')
 const routes = require('./src/routes')
-const umzug = require('umzug')
+const umzug = require('./src/database/umzug')
 
 
 const app = express()
 
-(async () => {
+//(async () => {
     // Checks migrations and run them if they are not already applied. To keep
     // track of the executed migrations, a table (and sequelize model) called SequelizeMeta
     // will be automatically created (if it doesn't exist already) and parsed.
-    await umzug.up();
-  })();
+    //await umzug.up();
+ // })();
 
 app.use(express.json())
 app.use(routes)
 
-const server = app.listen(process.env.PORT || 8090, () => {
+const server = app.listen(process.env.PORT || 8090, async () => {
+
+    await umzug.up();
     var host = server.address().address;
     var port = server.address().port;
 
--- src/database/umzug.js
+++ src/database/umzug.js
@@ -1,8 +1,9 @@
 const sequelize = require('./index')
 const { Umzug, SequelizeStorage } = require('umzug');
+const path = require('path')
 
 const umzug = new Umzug({
-    migrations: { glob: 'migrations/*.js' },
+    migrations: { glob: path.resolve(__dirname,'migrations', '*.js')},
     context: sequelize.getQueryInterface(),
     storage: new SequelizeStorage({ sequelize }),
     logger: console,
